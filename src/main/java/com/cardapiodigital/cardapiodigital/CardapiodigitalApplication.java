c5e9de80d8f0b14c9174f40d2e42bc716ff2ea21
Thiago Aguiar Cessin <thiagoacessin@gmail.com>
2021-04-23 17:25:33 -0300

    Commit inicial

---
+++ src/main/java/com/cardapiodigital/cardapiodigital/CardapiodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.cardapiodigital.cardapiodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiodigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardapiodigital/cardapiodigital/CardapiodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardapiodigital.cardapiodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapiodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
