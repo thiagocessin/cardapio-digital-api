4fe2c7e0c17dc73bcf9988824b82e9f83eff6de8
danielcessin <danielcessin@gmail.com>
2021-05-18 09:34:38 -0300

    Criando cors filters configurations

--- src/main/java/br/com/cardapiodigital/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/cardapiodigital/configuration/SecurityConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.cardapiodigital.configuration;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,6 +10,9 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -28,7 +34,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic()
+		http.cors()
+			.and()
+			.httpBasic()
 			.and()
 			.authorizeRequests()
 			.antMatchers(HttpMethod.POST)
@@ -37,6 +45,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 			.and()
 			.csrf().disable()
 			.formLogin();
+			
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 
